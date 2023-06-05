5f4162f3b6bbdd5887c1118687cade620ce570f5
Muhammed Fazil <mhdf088@gmail.com>
2023-06-05 11:39:55 +0530

    Sign up page UI will be created

---
+++ src/Components/SignUp/SignUp.js
@@ -0,0 +1,55 @@
+import React,{useState} from 'react';
+import './SignUp.css';
+
+function SignUp() {
+  const [loginIs,setLoginIs] = useState(false)
+  const [signupIs,setSignUpIs] = useState(false)
+  return (
+    <div className='signupPage'>
+      <div className={`login ${loginIs && 'signup_bg'}`}>
+        {signupIs && <div className={`signupInfo ${signupIs && 'visible2'}`}>
+          <form id='form' action="">
+            <div className='inputFields'>
+              <label htmlFor="">Name</label>
+              <input type="text" required />
+            </div>
+            <div className='inputFields'>
+              <label htmlFor="">E-mail</label>
+              <input type="email" required  />
+            </div>
+           <div className='inputFields'>
+              <label htmlFor="">Password</label>
+              <input type="password" required  />
+           </div>
+           <button className='Btn' type='submit'>Sign Up</button>
+          </form>
+          </div>}
+        <h2 className={`loginClick`} onClick={()=>{
+          setLoginIs(!loginIs)
+        }} >Login</h2>
+        <p>Already have an account</p>
+      </div>
+      <div className={`signup ${signupIs && 'login_bg'}`}>
+        {loginIs && <div className={`loginInfo ${loginIs&&'visible1'}`}>
+          <form className="loginItems">
+            <div className='inputFields'>
+              <label htmlFor="">User Name</label>
+              <input type="text" required />
+            </div>  
+            <div className='inputFields'>
+              <label htmlFor="">Password</label>
+              <input type="password" required />
+            </div>
+            <button className='Btn'>Sign In</button>
+          </form>
+        </div>}
+        <h2 className='signupClick' onClick={()=>{
+          setSignUpIs(!signupIs)
+        }} >SignUp</h2>
+        <p>Don't have an account?</p>
+      </div>
+    </div>
+  )
+}
+
+export default SignUp
